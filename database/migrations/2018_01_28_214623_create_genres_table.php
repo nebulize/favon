e6d36968b8a85735f7eef5e8fc5b0a5b4e67bfaf
D2Allaire <elishawitte@gmail.com>
2018-02-02 18:06:12 +0100

    Add unique constraint to genre name

--- database/migrations/2018_01_28_214623_create_genres_table.php
+++ database/migrations/2018_01_28_214623_create_genres_table.php
@@ -15,7 +15,7 @@ class CreateGenresTable extends Migration
     {
         Schema::create('genres', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('name')->unique();
         });
     }
 
