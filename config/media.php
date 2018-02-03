2266e71ad66cce7ea0a8a9234ac709b08b4a5fd6
D2Allaire <elishawitte@gmail.com>
2018-02-03 13:25:42 +0100

    Add API config values

---
+++ config/media.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'tvdb_api_key' => env('TVDB_API_KEY'),
+    'tvdb_user_key' => env('TVDB_USER_KEY'),
+    'tvdb_user_name' => env('TVDB_USER_NAME'),
+    'tvdb_url' => 'https://api.thetvdb.com',
+
+    'omdb_api_key' => env('OMDB_API_KEY'),
+    'omdb_url' => 'http://www.omdbapi.com',
+
+    'tmdb_api_key' => env('TMDB_API_KEY'),
+    'tmdb_url' => 'https://api.themoviedb.org/3',
+
+    'image_base_path' => 'http://image.tmdb.org/t/p/',
+    'poster_sizes' => ['w92', 'w342'],
+    'banner_sizes' => ['w1280'],
+];
