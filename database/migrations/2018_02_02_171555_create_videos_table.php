05dfa08d63e6be2cf0e82fdb624d5bf66c42c4aa
Elisha Witte <elishawitte@gmail.com>
2018-02-14 20:52:14 +0000

    Apply fixes from StyleCI

--- database/migrations/2018_02_02_171555_create_videos_table.php
+++ database/migrations/2018_02_02_171555_create_videos_table.php
@@ -1,1 +1,38 @@
-<?phpuse Illuminate\Support\Facades\Schema;use Illuminate\Database\Schema\Blueprint;use Illuminate\Database\Migrations\Migration;class CreateVideosTable extends Migration{    /**     * Run the migrations.     *     * @return void     */    public function up()    {        Schema::create('videos', function (Blueprint $table) {            $table->increments('id');            $table->string('name');            $table->string('key');            $table->string('type')->nullable();            $table->integer('tv_season_id')->unsigned();            $table->foreign('tv_season_id')->references('id')->on('tv_seasons')->onDelete('cascade');            $table->timestamps();        });    }    /**     * Reverse the migrations.     *     * @return void     */    public function down()    {        Schema::dropIfExists('videos');    }}
+<?php
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVideosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+
+    public function up()
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('key');
+            $table->string('type')->nullable();
+            $table->integer('tv_season_id')->unsigned();
+            $table->foreign('tv_season_id')->references('id')->on('tv_seasons')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+
+    public function down()
+    {
+        Schema::dropIfExists('videos');
+    }
+}
