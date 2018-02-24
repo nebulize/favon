2865a8a193930aed9815f5e7e01f0db4cc5c2289
Elisha Witte <elishawitte@gmail.com>
2018-02-24 02:32:50 +0100

    Futher adjust seasonal index filter

--- app/Repositories/TvSeasonRepository.php
+++ app/Repositories/TvSeasonRepository.php
@@ -81,24 +81,32 @@ class TvSeasonRepository implements RepositoryContract
         // Get seasonal index
         if (isset($parameters['seasonal']) && $parameters['seasonal'] === true) {
             $season = $parameters['season'];
-            $query = $query->join('tv_shows', 'tv_seasons.tv_show_id', '=', 'tv_shows.id');
-            if (! isset($parameters['season'])) {
-                throw new InvalidArgumentException('Requesting seasonal index, but no season specified.');
-            }
-
-            return $query
+            $query = $query
+                ->join('tv_shows', 'tv_seasons.tv_show_id', '=', 'tv_shows.id')
                 ->where('tv_seasons.season_id', '=', $season->id)
                 ->where(function ($q) use ($season) {
                     $q->where('tv_shows.imdb_votes', '>=', 2000);
+                    // Only filter by popularity if it's a current or future season. That way we also get shows
+                    // that have not yet premiered
                     if (Carbon::now()->gt($season->end_date) === false) {
                         $q->orWhere('tv_shows.popularity', '>=', 10);
                     }
-                })
+                });
+            // For current or future seasons, only query english language shows. This filters out a few good
+            // international shows (e.g. `Dark`), but overall we get rid of all the other crap. They will appear
+            // once the season is in the past.
+            if (Carbon::now()->gt($season->end_date) === true) {
+                $query = $query->whereIn('tv_shows.id', function ($q) {
+                    $q->select('tv_show_id')->from('language_tv_show')->where('language_code', 'en');
+                });
+            } else {
+                $query = $query->whereNotIn('tv_shows.id', function ($q) {
+                    $q->select('tv_show_id')->from('country_tv_show')->where('country_code', 'IN');
+                });
+            }
+            $query = $query
                 ->whereNotIn('tv_shows.id', function ($q) {
                     $q->select('tv_show_id')->from('genre_tv_show')->whereIn('genre_id', [3, 11, 12, 13, 22]);
-                })
-                ->whereNotIn('tv_shows.id', function ($q) {
-                    $q->select('tv_show_id')->from('country_tv_show')->where('country_code', 'IN');
                 })
                 ->orderBy('tv_shows.popularity', 'DESC')
                 ->with('tvShow.genres')
