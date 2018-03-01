9f37fb49c9f63108d6a778c8e5f9f7c250eaa6b6
Elisha Witte <elishawitte@gmail.com>
2018-03-01 12:46:22 +0100

    Rename migration to avoid class name conflicts

--- database/migrations/2018_02_28_195410_add_number_index_to_tv_seasons_table.php
+++ database/migrations/2018_02_28_195410_add_number_index_to_tv_seasons_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddIndexToTvSeasonsTable extends Migration
+class AddNumberIndexToTvSeasonsTable extends Migration
 {
     /**
      * Run the migrations.
