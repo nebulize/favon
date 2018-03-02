30cf960e82d78f8b10d44ddfbbc18d35e002be6c
Elisha Witte <elishawitte@gmail.com>
2018-03-02 15:28:01 +0100

    Add styles for placeholder image

--- resources/views/tv/seasonal/index.blade.php
+++ resources/views/tv/seasonal/index.blade.php
@@ -50,6 +50,10 @@
                                         <img src="http://image.tmdb.org/t/p/w342{{ $tvSeason->poster }}">
                                     @elseif($tvSeason->tvShow->poster)
                                         <img src="http://image.tmdb.org/t/p/w342{{ $tvSeason->tvShow->poster }}">
+                                    @else
+                                        <div class="poster__placeholder">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" fill="none" stroke="#b5b5b5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-image"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><circle cx="8.5" cy="8.5" r="1.5"></circle><polyline points="21 15 16 10 5 21"></polyline></svg>
+                                        </div>
                                     @endif
                                 </div>
                                 <div class="body__description">
