bf54f43d159de6193f3a53532ef891f9ce0cf30d
Elisha Witte <elishawitte@gmail.com>
2018-04-15 19:53:58 +0200

    Temporarily remove episode count and network information (timeout)

--- resources/assets/js/seasonal/components/TvSeasonCard.vue
+++ resources/assets/js/seasonal/components/TvSeasonCard.vue
@@ -43,10 +43,9 @@
         <div class="flex-left">
           <span>{{ tv_season.first_aired_string }}</span>
         </div>
-        <div class="flex-center">
-          <span>{{ tv_season.tv_episodes_count }} Eps</span>
-          <span class="footer__network">{{ tv_season.tv_show.networks[0].name }}</span>
-        </div>
+        <!--<div class="flex-center">-->
+        <!--<span class="footer__network">{{ tv_season.tv_show.networks[0].name }}</span>-->
+        <!--</div>-->
         <div class="flex-right">
           <img src="/images/imdb.svg">
           <span v-if="tv_season.tv_show.imdb_score === 0">N/A</span>
