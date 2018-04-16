ac225f878ef08bd881bd607b6ccd1cef996f8d7b
Elisha Witte <elishawitte@gmail.com>
2018-04-16 13:33:31 +0200

    Fix mobile nav not working outside seasonal view

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,6 +9,7 @@
 @include('layouts.partials.footer')
 @section('scripts')
     <script src="{{ asset_path('js/favon.vendor.js') }}"></script>
+    <script src="{{ asset_path('js/favon.app.js') }}"></script>
 @show
 </body>
 </html>
