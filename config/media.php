01fd7b617466bd75c5fc1793feb14a89ee58ea3b
Elisha Witte <elishawitte@gmail.com>
2018-04-16 14:06:16 +0200

    Load images over HTTPS

--- config/media.php
+++ config/media.php
@@ -12,7 +12,7 @@ return [
     'tmdb_api_key' => env('TMDB_API_KEY'),
     'tmdb_url' => 'https://api.themoviedb.org/3',
 
-    'image_base_path' => 'http://image.tmdb.org/t/p/',
+    'image_base_path' => 'https://image.tmdb.org/t/p/',
     'poster_sizes' => ['w92', 'w342'],
     'banner_sizes' => ['w1280'],
     'profile_sizes' => ['w185'],
