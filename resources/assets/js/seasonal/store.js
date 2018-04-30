eefa443f1f5e352225c6cdc0edb69abfabc20758
Elisha Witte <elishawitte@gmail.com>
2018-04-30 20:22:58 +0200

    Slightly adjust default filters

--- resources/assets/js/seasonal/store.js
+++ resources/assets/js/seasonal/store.js
@@ -10,9 +10,9 @@ export default {
       },
       genres: {
         all: false,
-        values: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 14, 15, 16, 17, 19, 20, 21, 24, 25, 26, 27, 28, 30],
+        values: [1, 2, 4, 5, 6, 7, 8, 9, 10, 15, 16, 17, 19, 20, 21, 24, 25, 26, 27, 28, 30],
       },
-      languages: ['en', 'ja'],
+      languages: ['en'],
     },
     season: {},
     tv_seasons: [],
