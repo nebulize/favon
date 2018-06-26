baebf002e2d33d09361e95972e628a7db30804a4
Elisha Witte <elishawitte@gmail.com>
2018-06-26 02:06:39 +0200

    Fix average tv show rating taking into account non-rated (0) seasons

--- app/Repositories/UserTvSeasonRepository.php
+++ app/Repositories/UserTvSeasonRepository.php
@@ -44,8 +44,8 @@ class UserTvSeasonRepository
                 ->join('tv_seasons', 'user_tv_season.tv_season_id', '=', 'tv_seasons.id')
                 ->where('tv_show_id', '=', $parameters['tv_show_id']);
         }
-
-        return $query->get();
+        
+        return $query->select('user_tv_season.*')->get();
     }
 
     public function count(array $parameters = [])
