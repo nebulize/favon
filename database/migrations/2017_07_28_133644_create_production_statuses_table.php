18bbb6b314b880a49fb2e05df49205446191fb1f
Elisha Witte <elisha.witte@antwerpes.de>
2018-07-30 15:42:43 +0200

    Rename tv status to production status

--- database/migrations/2017_07_28_133644_create_production_statuses_table.php
+++ database/migrations/2017_07_28_133644_create_production_statuses_table.php
@@ -4,14 +4,14 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTvStatusesTable extends Migration
+class CreateProductionStatusesTable extends Migration
 {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('tv_statuses', function (Blueprint $table) {
+        Schema::create('production_statuses', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
         });
@@ -22,6 +22,6 @@ class CreateTvStatusesTable extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tv_statuses');
+        Schema::dropIfExists('production_statuses');
     }
 }
