2356001b47906da25677717ad53b0e471a9eebac
elishawitte <elisha.witte@antwerpes.de>
2018-07-31 13:01:58 +0200

    Remove code duplication

--- app/Media/Commands/Initialize/FetchPersons.php
+++ app/Media/Commands/Initialize/FetchPersons.php
@@ -3,6 +3,7 @@
 namespace Favon\Media\Commands\Initialize;
 
 use Carbon\Carbon;
+use Favon\Media\Services\FetchingService;
 use Illuminate\Console\Command;
 use Favon\Media\Jobs\FetchPerson;
 
@@ -23,75 +24,33 @@ class FetchPersons extends Command
     protected $description = 'Fetch all persons from the TMDB API and store them in database (~100h!)';
 
     /**
+     * @var FetchingService
+     */
+    protected $fetchingService;
+
+    /**
+     * FetchPersons constructor.
+     * @param FetchingService $fetchingService
+     */
+    public function __construct(FetchingService $fetchingService)
+    {
+        parent::__construct();
+        $this->fetchingService = $fetchingService;
+    }
+
+    /**
      * Execute the console command.
      */
     public function handle(): void
     {
         $this->line('Fetching latest person export from TMDB...');
-        $this->fetchFile();
+        $this->fetchingService->fetchFile('person');
         $this->line('Extracting...');
-        $this->extract();
+        $this->fetchingService->extract('person');
         $this->info('Successfully extracted the person list.');
         $this->line('Queueing fetching jobs...');
-        $this->fetchPersons();
-        $this->deleteFiles();
+        $this->fetchingService->fetch('person', FetchPerson::class);
+        $this->fetchingService->deleteFiles('person');
         $this->info('Deleted downloaded files. The fetching of persons is queued and will take around 100 hours to complete');
-    }
-
-    /**
-     * Fetch daily export files from TMDB.
-     */
-    protected function fetchFile(): void
-    {
-        $date = Carbon::now();
-        $url = 'http://files.tmdb.org/p/exports/person_ids_'.$date->format('m_d_Y').'.json.gz';
-        set_time_limit(0);
-        $fp = fopen(storage_path('api/person_ids.json.gz'), 'wb+');
-        $curlCh = curl_init($url);
-        curl_setopt($curlCh, CURLOPT_TIMEOUT, 50);
-        curl_setopt($curlCh, CURLOPT_FILE, $fp);
-        curl_setopt($curlCh, CURLOPT_FOLLOWLOCATION, true);
-        curl_exec($curlCh);
-        curl_close($curlCh);
-        fclose($fp);
-    }
-
-    /**
-     * Extract the gzipped file.
-     */
-    protected function extract(): void
-    {
-        $sfp = gzopen(storage_path('api/person_ids.json.gz'), 'rb');
-        $fp = fopen(storage_path('api/person_ids.json'), 'wb');
-        while (! gzeof($sfp)) {
-            $string = gzread($sfp, 4096);
-            fwrite($fp, $string, \strlen($string));
-        }
-        gzclose($sfp);
-        fclose($fp);
-    }
-
-    /**
-     * Go line by line through the extracted file and dispatch a fetch job for each entry.
-     */
-    protected function fetchPersons(): void
-    {
-        $handle = fopen(storage_path('api/person_ids.json'), 'rb');
-        while (! feof($handle)) {
-            $entry = json_decode(trim(fgets($handle)));
-            if (\is_object($entry) && property_exists($entry, 'id')) {
-                FetchPerson::dispatch($entry->id);
-            }
-        }
-        fclose($handle);
-    }
-
-    /**
-     * Delete downloaded files.
-     */
-    protected function deleteFiles(): void
-    {
-        unlink(storage_path('api/person_ids.json.gz'));
-        unlink(storage_path('api/person_ids.json'));
     }
 }
