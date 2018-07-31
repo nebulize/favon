e3b1e06200c6e34bf0c6359fb538889efa17750d
elishawitte <elisha.witte@antwerpes.de>
2018-07-31 13:01:58 +0200

    Remove code duplication

--- app/Television/Commands/Initialize/FetchShows.php
+++ app/Television/Commands/Initialize/FetchShows.php
@@ -2,7 +2,7 @@
 
 namespace Favon\Television\Commands\Initialize;
 
-use Carbon\Carbon;
+use Favon\Media\Services\FetchingService;
 use Illuminate\Console\Command;
 use Favon\Television\Jobs\FetchShow;
 
@@ -23,75 +23,33 @@ class FetchShows extends Command
     protected $description = 'Fetch all TV shows from TMDB and store them in database';
 
     /**
+     * @var FetchingService
+     */
+    protected $fetchingService;
+
+    /**
+     * FetchShows constructor.
+     * @param FetchingService $fetchingService
+     */
+    public function __construct(FetchingService $fetchingService)
+    {
+        parent::__construct();
+        $this->fetchingService = $fetchingService;
+    }
+
+    /**
      * Execute the command.
      */
     public function handle(): void
     {
         $this->line('Fetching latest tv shows export from TMDB...');
-        $this->fetchFile();
+        $this->fetchingService->fetchFile('tv_series');
         $this->line('Extracting...');
-        $this->extract();
+        $this->fetchingService->extract('tv_series');
         $this->info('Successfully extracted the tv show list.');
         $this->line('Queueing fetching jobs...');
-        $this->fetchShows();
-        $this->deleteFiles();
+        $this->fetchingService->fetch('tv_series', FetchShow::class);
+        $this->fetchingService->deleteFiles('tv_series');
         $this->info('Deleted downloaded files. The fetching of tv shows is queued and will take around 80 hours to complete');
-    }
-
-    /**
-     * Fetch the daily export file for TV shows from TMDB and store it locally.
-     */
-    protected function fetchFile(): void
-    {
-        $date = Carbon::now();
-        $url = 'http://files.tmdb.org/p/exports/tv_series_ids_'.$date->format('m_d_Y').'.json.gz';
-        set_time_limit(0);
-        $fp = fopen(storage_path('api/tv_series_ids.json.gz'), 'wb+');
-        $curlCh = curl_init($url);
-        curl_setopt($curlCh, CURLOPT_TIMEOUT, 50);
-        curl_setopt($curlCh, CURLOPT_FILE, $fp);
-        curl_setopt($curlCh, CURLOPT_FOLLOWLOCATION, true);
-        curl_exec($curlCh);
-        curl_close($curlCh);
-        fclose($fp);
-    }
-
-    /**
-     * Extract the downloaded export file.
-     */
-    protected function extract(): void
-    {
-        $sfp = gzopen(storage_path('api/tv_series_ids.json.gz'), 'rb');
-        $fp = fopen(storage_path('api/tv_series_ids.json'), 'wb');
-        while (! gzeof($sfp)) {
-            $string = gzread($sfp, 4096);
-            fwrite($fp, $string, \strlen($string));
-        }
-        gzclose($sfp);
-        fclose($fp);
-    }
-
-    /**
-     * Go through the extracted list line by line and dispatch a job for each entry.
-     */
-    protected function fetchShows(): void
-    {
-        $handle = fopen(storage_path('api/tv_series_ids.json'), 'rb');
-        while (! feof($handle)) {
-            $entry = json_decode(trim(fgets($handle)));
-            if (\is_object($entry) && property_exists($entry, 'id')) {
-                FetchShow::dispatch($entry->id);
-            }
-        }
-        fclose($handle);
-    }
-
-    /**
-     * Delete the downloaded files since we don't need them anymore.
-     */
-    protected function deleteFiles(): void
-    {
-        unlink(storage_path('api/tv_series_ids.json.gz'));
-        unlink(storage_path('api/tv_series_ids.json'));
     }
 }
