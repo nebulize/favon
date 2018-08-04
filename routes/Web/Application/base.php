428ffb0739c104f8441870dc2147a13ecc80922d
Elisha Witte <elishawitte@gmail.com>
2018-08-04 00:32:54 +0000

    Apply fixes from StyleCI

--- routes/Web/Application/base.php
+++ routes/Web/Application/base.php
@@ -9,8 +9,7 @@
 |
 */
 
-
-Route::get('/', function() {
+Route::get('/', function () {
     return redirect()->route('television.seasonal.show');
 });
 Route::any('adminer', '\Miroc\LaravelAdminer\AdminerController@index');
